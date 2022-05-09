9482f8afddaec32a2b9013e75c4230a763673cad
DolgihDmitriy <navis1mplegod@mail.ru>
2022-05-09 20:13:27 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="serializers",
+    version="1.0",
+    author="dima",
+    author_email="dimadol03@gmail.com",
+    description="json, toml, yaml serializers",
+    keywords="json toml yaml serializer",
+    url="https://pypi.org/project/serializers/"
+)
